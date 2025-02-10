9245d60cc77c907c4ceb5f589fa3c4c27684035f
Flavio Gonzaga <fbgonzaga@gmail.com>
2025-02-10 02:23:52 -0300

    Secao 1

---
+++ section1/springsecsection1/src/main/java/com/easybytes/springsecsection1/EazyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.easybytes.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan("com.easybytes.springsecsection1.controller")
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
